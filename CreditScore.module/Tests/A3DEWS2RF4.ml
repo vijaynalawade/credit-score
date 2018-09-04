acaa947f956e0c0a035bf9823ee10103bfdec284
Vijay Nalawade <vnalawad@tibco.com>
2018-09-03 19:00:03 -0700

    Service updates

--- CreditScore.module/Tests/A3DEWS2RF4.ml
+++ CreditScore.module/Tests/A3DEWS2RF4.ml
@@ -3,4 +3,5 @@
   <CurrentLoadedTest href="platform:/resource/CreditScore.module/Tests/CreditScoreTest.bwt#/"/>
   <EmulationDataFile>ACED000574001443726564697453636F726554657374732E627774</EmulationDataFile>
   <EmulationDataFile>ACED000574001343726564697453636F7265546573742E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574001B43726561746543616D706169676E4C65616473546573742E627774</EmulationDataFile>
 </emulation:BWTFile>
